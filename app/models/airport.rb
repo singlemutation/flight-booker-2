8bcfc518ae5f405411b00d729789c2d798b3b5e7
sam <164826575+singlemutation@users.noreply.github.com>
2024-09-15 10:25:49 -0400

    Fix association of flights to airports

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,4 @@
 class Airport < ApplicationRecord
-  has_many :departing_flights, inverse_of: "departure_airport"
-  has_many :arriving_flights, inverse_of: "arrival_airport"
+  has_many :departing_flights, class_name: "Flight", inverse_of: "departure_airport"
+  has_many :arriving_flights, class_name: "Flight", inverse_of: "arrival_airport"
 end
