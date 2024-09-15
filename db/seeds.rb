4ec8a03a39c28b2f102b686de54fa18ae197bb56
sam <164826575+singlemutation@users.noreply.github.com>
2024-09-15 10:10:39 -0400

    Add airport and flight models and associate them

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Airport.find_or_create_by!(airport_code: 'SFO', airport_name: 'San Francisco')
+Airport.find_or_create_by!(airport_code: 'DEN', airport_name: 'Denver')
+Airport.find_or_create_by!(airport_code: 'ORD', airport_name: 'Chicago O\'Hare')
+Airport.find_or_create_by!(airport_code: 'ATL', airport_name: 'Atlanta')
+Airport.find_or_create_by!(airport_code: 'DTW', airport_name: 'Detroit')
+Airport.find_or_create_by!(airport_code: 'LAX', airport_name: 'Los Angeles')
